3d9a5349b06746816e531f0b6fe19d20eb103d9b
YejiGong <87148580+YejiGong@users.noreply.github.com>
2021-11-14 19:33:52 +0900

    Create 1912.py
    boj 1912 solved

---
+++ boj/1912.py
@@ -0,0 +1,7 @@
+import sys
+n=int(sys.stdin.readline())
+num=list(map(int,sys.stdin.readline().split()))
+dp=[num[0]]
+for i in range(n-1):
+    dp.append(max(dp[i]+num[i+1],num[i+1]))
+print(max(dp))
