0749ab1157d637070d0c8fd9b08612f10dd543dc
YejiGong <87148580+YejiGong@users.noreply.github.com>
2022-07-19 21:26:44 +0900

    Create 1927.py
    boj 1927 solved

---
+++ boj/1927.py
@@ -0,0 +1,13 @@
+import heapq
+import sys
+N=int(sys.stdin.readline())
+heap = []
+for i in range(N):
+    m = int(sys.stdin.readline())
+    if m == 0 :
+        if len(heap)!=0:
+            print(heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, m)
