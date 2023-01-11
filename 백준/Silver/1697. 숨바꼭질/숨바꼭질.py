316585653478ca2b22db2c8842c988547ee00872
YejiGong <yejigong00@gmail.com>
2023-01-11 12:13:27 +0900

    [Silver I] Title: 숨바꼭질, Time: 132 ms, Memory: 35720 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,15 @@
+from collections import deque
+N, K = map(int,input().split())
+MAX = 10**5
+dist = [0 for _ in range(MAX+1)]
+queue = deque()
+queue.append(N)
+while queue:
+    x = queue.popleft()
+    if x==K:
+        print(dist[x])
+        break
+    for nx in (x-1, x+1, 2*x):
+        if 0<=nx<=MAX and dist[nx]==0:
+            dist[nx] = dist[x]+1
+            queue.append(nx)
