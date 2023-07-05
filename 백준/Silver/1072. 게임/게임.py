f8ea086fcfbe467b0d2f45d8a28e21359624f2e1
YejiGong <yejigong00@gmail.com>
2023-07-05 13:17:31 +0900

    [Silver III] Title: 게임, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.py
@@ -0,0 +1,17 @@
+X, Y = map(int,input().split())
+Z = (Y*100)//X
+if Z>=99:
+    print(-1)
+else:
+    ans = 0
+    left = 1
+    right = X
+
+    while left<=right:
+        mid = (left+right)//2
+        if (Y+mid)*100 // (X+mid)<=Z:
+            left = mid+1
+        else:
+            ans = mid
+            right = mid-1
+    print(ans)
