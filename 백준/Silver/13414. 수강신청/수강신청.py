bec8f7e6b22a984dd451d78879079d6355a8c058
YejiGong <yejigong00@gmail.com>
2024-03-22 17:56:24 +0900

    [Silver III] Title: 수강신청, Time: 540 ms, Memory: 75456 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.py
+++ 백준/Silver/13414. 수강신청/수강신청.py
@@ -1,16 +1,19 @@
 import sys
 input = sys.stdin.readline
 K,L = map(int,input().split())
+arr = []
 student_set={}
 #1<=L<=500,000, 0<=K<=100,000에 1초 : O(N)
 for i in range(L):
-    std = input()
-    if(std in student_set.keys()):
-        student_set.pop(std)
+    std = input().rstrip()
     student_set[std] = i
+    arr.append(std)
 check = 0
-arr = list(student_set.items())
-for i in range(min(K, len(arr))):
-    print(arr[i][0].rstrip())
+for i in range(L):
+    if(student_set[arr[i]]==i):
+        check += 1
+        print(arr[i])
+    if(check==K):
+        break
 
 
