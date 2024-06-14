809d55cee0bb48a623b3d9fe4a2af84fd6c2136d
YejiGong <yejigong00@gmail.com>
2024-06-14 22:35:49 +0900

    [Gold V] Title: 동전 1, Time: 160 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,9 @@
+n,k = map(int,input().split())
+coins = [int(input()) for _ in range(n)]
+coins.sort()
+dp=[0 for _ in range(k+1)]
+dp[0] = 1
+for i in coins:
+    for j in range(i, k+1):
+        dp[j] += dp[j-i]
+print(dp[k])
