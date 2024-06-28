4780ee83242f49d51bada1a6b75365d82f91337b
YejiGong <yejigong00@gmail.com>
2024-06-28 23:27:59 +0900

    [Bronze V] Title: 몇개고?, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27294. 몇개고？/몇개고？.py
@@ -0,0 +1,16 @@
+t,s = map(int,input().split())
+if(t<=11):
+    if(s==1):
+        print(280)
+    else:
+        print(280)
+elif(12<=t<=16):
+    if(s==1):
+        print(280)
+    else:
+        print(320)
+else:
+    if(s==1):
+        print(280)
+    else:
+        print(280)
