3859cb108b3865c0dae544936501034781d0e5fa
YejiGong <yejigong00@gmail.com>
2024-07-07 20:26:41 +0900

    [Silver I] Title: IOIOI, Time: 372 ms, Memory: 52448 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,15 +1,29 @@
 N=int(input())
 M=int(input())
 S=input()
-lenth = 2*N+1
-keyword = ''
-for i in range(lenth):
-    if i%2==0:
-        keyword+='I'
+result = []
+for i in range(1,M-1):
+    if S[i]=='O' and S[i-1]=='I' and S[i+1]=='I':
+        result.append(i)
+if(N==1):
+    print(len(result))
+else:
+    if(len(result)>0):
+        idx = result[0]
+        cnt = 1
+        res = 0
+        for i in range(1,len(result)):
+            if result[i]-idx == 2:
+                idx = result[i]
+                cnt+=1
+            else:
+                #4일때 N=2면 3개,4일때 N=3이면 2개, 4일때 N=4면 1개.
+                if(cnt>=N):
+                    res += cnt-N+1
+                idx = result[i]
+                cnt = 1
+        if(cnt>=N):
+            res += cnt-N+1
+        print(res)
     else:
-        keyword+='O'
-result = 0
-for i in range(M-lenth+1):
-    if S[i:i+lenth] == keyword:
-        result+=1
-print(result)
+        print(0)
